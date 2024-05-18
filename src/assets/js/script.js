67f5d8799228d250ce87ca80890d2602ab13c0bd
sena0514 <andy.cha0514@gmail.com>
2024-05-18 15:21:43 +0900

    修正

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -7,7 +7,14 @@ jQuery(function ($) {
     return false;
   });
 
-
+    //ドロワーリンク
+    $(".p-drawer__list a").on("click", function () {
+      $(".p-drawer__icon").removeClass("js-open");
+      $(".p-drawer__menu").removeClass("js-open");
+      $(".p-drawer__bg").removeClass("js-open");
+      $("body").removeClass("no-scroll");
+      //return false;
+    });
   
   //画面幅を広げたら自動で閉じるように
   $(window).resize(function () {
@@ -28,14 +35,28 @@ const smoothScrollTriggers = document.querySelectorAll('a[href^="#"]');
 smoothScrollTriggers.forEach((smoothScrollTrigger) => {
   smoothScrollTrigger.addEventListener("click", (e) => {
     e.preventDefault();
-    const hrefLink = smoothScrollTrigger.getAttribute("href");
-    const targetElement = document.getElementById(hrefLink.replace("#", ""));
+    const href = smoothScrollTrigger.getAttribute("href");
+    // hrefが"#"または""の場合、スクロールさせない
+    if (href === "#" || href === "") return;
+
+    const targetElement = document.querySelector(href);
+    // ターゲット要素が存在しない場合、スクロールさせない
+    if (!targetElement) return;
+
     const rectTop = targetElement.getBoundingClientRect().top;
     const offset = window.pageYOffset;
-    const scrollTarget = rectTop + offset;
+    const gap = 20; // スクロール位置の微調整に使用
+    const target = rectTop + offset - gap;
+
     window.scrollTo({
-      top: scrollTarget,
+      top: target,
       behavior: "smooth",
     });
+
+    // ドロワーメニューとアイコンの状態を閉じる
+    $(".p-drawer__icon").removeClass("js-open");
+    $(".p-drawer__menu").removeClass("js-open");
+    $("body").removeClass("no-scroll");
   });
 });
+
